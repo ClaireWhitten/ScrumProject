84a08c41d0d52efe7447a15fe1ba2cb8bc0c8a45
Claire Whitten <claire.whitten@student.kdg.be>
2021-07-16 14:17:12 +0200

    Add project files.

---
+++ ScrumProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScrumProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Databank tabellen opstellen voor rechten");
+        }
+    }
+}
